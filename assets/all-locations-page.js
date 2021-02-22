1c3dccc26f1b8fd9ca42b677b85828e0ac731632
Antoni Tomov <antotomov@gmail.com>
2021-02-22 15:33:43 +0200

    making a few adjustments on mainHeader

--- assets/all-locations-page.js
+++ assets/all-locations-page.js
@@ -16,36 +16,36 @@ function printAllLocationsPage(location, allStays) {
     info.innerText = `Stays in ${location}`;
 
     allStayInLocation.forEach(el => {
-            let newCard = createEl('div', 'className', 'new-card');
-            let imgDiv = createEl('img', 'className', 'card-img');
-            imgDiv.src = `${el.image}`;
-            // imgDiv.src = `images/cosy-stay.jpeg`;
-            let ratingSpan = createEl('span', 'className', 'rating-span');
-            let starIcon = createEl('img', 'className', 'star-icon');
-            starIcon.src = 'assets/images/icons/red-star.png';
-            starIcon.alt = 'rating star';
-            let currentRating = createEl('span', 'className', 'current-rating');
-            currentRating.innerHTML = el.rating;
-            let usersVotes = createEl('span', 'className', 'users-votes');
-            usersVotes.innerHTML = `(${el.reviews})`
-            let descriptionContainer = createEl('div', 'className', 'description-container');
-            let cardTitle = createEl('div', 'className', 'card-title');
-            cardTitle.innerHTML = `${el.stayType} · ${el.location}`
-            let stayTitle = createEl('div', 'className', 'stay-title');
-            stayTitle.innerHTML = el.title.slice(0, 33) + '...';
+        let newCard = createEl('div', 'className', 'new-card');
+        let imgDiv = createEl('img', 'className', 'card-img');
+        imgDiv.src = `${el.images}`;
+        // imgDiv.src = `images/cosy-stay.jpeg`;
+        let ratingSpan = createEl('span', 'className', 'rating-span');
+        let starIcon = createEl('img', 'className', 'star-icon');
+        starIcon.src = 'assets/images/icons/red-star.png';
+        starIcon.alt = 'rating star';
+        let currentRating = createEl('span', 'className', 'current-rating');
+        currentRating.innerHTML = el.rating;
+        let usersVotes = createEl('span', 'className', 'users-votes');
+        usersVotes.innerHTML = `(${el.reviews})`
+        let descriptionContainer = createEl('div', 'className', 'description-container');
+        let cardTitle = createEl('div', 'className', 'card-title');
+        cardTitle.innerHTML = `${el.stayType} · ${el.location}`
+        let stayTitle = createEl('div', 'className', 'stay-title');
+        stayTitle.innerHTML = el.title.slice(0, 33) + '...';
 
-            if (el.reviews === 0) {
-                usersVotes.innerHTML === 'No reviews yet';
-                ratingSpan.append(usersVotes);
-            } else if (el.reviews < 3) {
-                currentRating.innerHTML === `${el.reviews} reviews`;
-                ratingSpan.append(starIcon, currentRating);
-            } else {
-                ratingSpan.append(starIcon, currentRating, usersVotes);
-            }
-            descriptionContainer.append(cardTitle, stayTitle);
-            newCard.append(imgDiv, ratingSpan, descriptionContainer);
-            staysContainer.append(newCard);
+        if (el.reviews === 0) {
+            usersVotes.innerHTML === 'No reviews yet';
+            ratingSpan.append(usersVotes);
+        } else if (el.reviews < 3) {
+            currentRating.innerHTML === `${el.reviews} reviews`;
+            ratingSpan.append(starIcon, currentRating);
+        } else {
+            ratingSpan.append(starIcon, currentRating, usersVotes);
+        }
+        descriptionContainer.append(cardTitle, stayTitle);
+        newCard.append(imgDiv, ratingSpan, descriptionContainer);
+        staysContainer.append(newCard);
     });
 
     buttonsContainer.append(typeOfPlaceButton, priceButton, moreFiltersButton);
