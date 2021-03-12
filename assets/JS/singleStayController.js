7bcb4746d0e40443a3d350171c849dc22ed7f035
Antoni Tomov <antotomov@gmail.com>
2021-03-12 23:50:28 +0200

    minor fixes on singleStayPage print

--- assets/JS/singleStayController.js
+++ assets/JS/singleStayController.js
@@ -1,16 +1,20 @@
 let allLocationsPage = getById('#allLocations');
 // allLocationsPage.addEventListener('load', )
 let selectedStayContainer = getById('targetStayContainer');
+let errorMessage = getById('errorMessageP');
 let currentLocation = localStorage.getItem('chosenLocation');
 let allStaysInLoc = staysManager.allStays.filter(el => el.location === currentLocation);
 let currentStay = staysManager.allStays.find(el => el.id === parseInt(localStorage.getItem('selectedStayId')));
 
-if (localStorage.getItem('selectedStayId')) printTargetStayPage(selectedStayContainer, currentStay);
-else selectedStayContainer.innerHTML = 'No stay selected';
+if (currentStay) {
+    errorMessage.innerHTML = '';
+    printTargetStayPage(selectedStayContainer, currentStay);
+} else errorMessage.innerHTML = 'No stay selected';
 
 window.addEventListener('click', (ev) => {
     let selectedStay = ev.target.closest('.new-card')
     if (selectedStay) {
+        errorMessage.innerHTML = '';
         let currentStayId = parseInt(selectedStay.id);
         staysManager.allStays.find(el => {
             if(el.id === currentStayId) {
